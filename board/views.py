3f78d409295399905e41d6ea03e5f3adc2266070
hykk-git <gratia1206@naver.com>
2025-03-19 00:15:32 +0000

    feat: views 주석 추가

--- board/views.py
+++ board/views.py
@@ -7,23 +7,25 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import *
 from .models import *
 
-### 메인 페이지 (루트)
+# 메인 페이지 (루트)
 class MainView(APIView):
     permission_classes = [AllowAny] 
 
     def get(self, request):
         return render(request, 'main.html')
 
-###회원가입 (POST: username, password)
+# 회원가입
 class SignupView(APIView):
     permission_classes = [AllowAny] 
 
     def get(self, request):
+        # 이미 로그인된 사용자는 main으로 이동
         if request.user.is_authenticated:
             return redirect('/')
         return render(request, 'signup.html')
 
     def post(self, request):
+        # 유저가 입력한 회원가입 정보(id, pw)를 DB에 저장
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -31,27 +33,30 @@ class SignupView(APIView):
         else:
             return render(request, 'signup.html', {"errors": serializer.errors})
 
-### 로그인 (JWT 토큰 발급, 헤더에 포함)
+# 로그인
 class LoginView(APIView):
     permission_classes = [AllowAny] 
 
     def get(self, request):
         return render(request, 'login.html')
 
-### 로그아웃 (JWT 방식에서는 클라이언트에서 토큰 삭제)
+# 로그아웃
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
 
+    # 로그아웃시 토큰 삭제 필요(클라이언트에서 처리)
     def post(self, request):
-        return Response({"message": "로그아웃 성공 (토큰 삭제는 클라이언트에서 처리)"}, status=200)
+        return Response({"message": "로그아웃 성공"}, status=200)
 
-### 게시판 조회 (JWT 인증 필수, 헤더에서 토큰 확인)
+# 게시판 조회
 class BoardView(APIView):
-    authentication_classes = [JWTAuthentication]  # JWT 인증 적용
-    permission_classes = [IsAuthenticated]  # 인증된 사용자만 접근 가능
+    # JWT 인증 적용, Authorization 헤더 확인
+    authentication_classes = [JWTAuthentication]  
+    # 인증된 사용자만 접근 가능한 View
+    permission_classes = [IsAuthenticated]  
 
     def get(self, request):
-        auth_header = request.headers.get("Authorization")  # 헤더에서 JWT 가져오기
+        auth_header = request.headers.get("Authorization")
         print("인증된 사용자:", request.user) 
 
         # 정상적으로 인증된 경우 게시판 데이터 반환
