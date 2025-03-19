7d1d0409576c09eb90e113d8837c2ef44e94e4f7
hykk-git <gratia1206@naver.com>
2025-03-19 01:12:18 +0000

    feat: 전체 주석 추가

--- board/serializers.py
+++ board/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Post
 
 class SignupSerializer(serializers.ModelSerializer):
